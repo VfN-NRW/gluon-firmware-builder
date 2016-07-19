54f72e0876867bd54af17306aa7b73cdd5b56a61
@RubenKelevra <ruben@vfn-nrw.de>
2016-07-19 03:19:28 +0200

    Update build_gluon.sh

--- build_gluon.sh
+++ build_gluon.sh
@@ -61,7 +61,7 @@ profiles="$(ls -1 site-modules/vfnnrw | grep -vE 'all|LICENSE|README.md|version'
 
 profiles="remscheid" #only build profile x
 
-branches="ar71xx-generic" # ar71xx-nand mpc85xx-generic x86-generic" #x86-kvm_guest
+branches="ar71xx-generic" # ipq806x ar71xx-nand mpc85xx-generic x86-generic " #x86-kvm_guest
 
 cd ./$GluonRelease
 
